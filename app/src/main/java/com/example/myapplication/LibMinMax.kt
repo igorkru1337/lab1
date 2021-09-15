bd7e97b3cff2d9eb3afb8ea27819b59188b942d5
igorkru1337 <krupenni@list.ru>
2021-09-15 15:10:38 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/LibMinMax.kt
@@ -0,0 +1,19 @@
+package com.example.myapplication
+
+class MinMax {
+
+	fun min(a: Int, b: Int): Int =
+		if (a < b) {
+			a
+		} else {
+			b
+		}
+
+	fun max(a: Int, b: Int): Int =
+		if (a > b) {
+			a
+		} else {
+			b
+		}
+}
+
---
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -0,0 +1,102 @@
+package com.example.myapplication
+
+import org.junit.Assert.*
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+
+	private val minMax = MinMax()
+
+	@Test
+	fun checkMinBetween3And6() {
+		val first = 3
+		val second = 6
+		val expected = 3
+
+		val actual = minMax.min(first, second)
+
+		assertEquals(expected, actual)
+	}
+
+	@Test
+	fun checkMinBetween4And4() {
+		val first = 4
+		val second = 4
+		val expected = 4
+
+		val actual = minMax.min(first, second)
+
+		assertEquals(expected, actual)
+	}
+
+	@Test
+	fun checkMinBetween7And2() {
+		val first = 7
+		val second = 2
+		val expected = 2
+
+		val actual = minMax.min(first, second)
+
+		assertEquals(expected, actual)
+	}
+
+	@Test
+	fun checkMinBetween0And4() {
+		val first = 0
+		val second = 4
+		val expected = 0
+
+		val actual = minMax.min(first, second)
+
+		assertEquals(expected, actual)
+	}
+
+	@Test
+	fun checkMaxBetween3And6() {
+		val first = 3
+		val second = 6
+		val expected = 6
+
+		val actual = minMax.max(first, second)
+
+		assertEquals(expected, actual)
+	}
+
+	@Test
+	fun checkMaxBetween4And4() {
+		val first = 4
+		val second = 4
+		val expected = 4
+
+		val actual = minMax.max(first, second)
+
+		assertEquals(expected, actual)
+	}
+
+	@Test
+	fun checkMaxBetween7And2() {
+		val first = 7
+		val second = 2
+		val expected = 7
+
+		val actual = minMax.max(first, second)
+
+		assertEquals(expected, actual)
+	}
+
+	@Test
+	fun checkMaxBetween0And4() {
+		val first = 0
+		val second = 4
+		val expected = 4
+
+		val actual = minMax.max(first, second)
+
+		assertEquals(expected, actual)
+	}
+}
